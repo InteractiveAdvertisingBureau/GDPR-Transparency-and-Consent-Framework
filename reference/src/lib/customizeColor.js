8257d1ee4ba7f119fc0e884d97dcfb8f06d8fa89
Hao Xu <hao@sharethis.com>
2018-05-31 14:52:03 -0700

    set default color

--- reference/src/lib/customizeColor.js
+++ reference/src/lib/customizeColor.js
@@ -1,61 +1,59 @@
 // color css template
 const colorCSS = `
-  <style>
-    .app_gdpr a {
-      color: default !important;
-    }
-    .app_gdpr a:hover {
-      color: #133214 !important;
-    }
-    .app_gdpr button {
-      border-color: default !important;
-    }
-    .app_gdpr .intro_acceptAll {
-      color: #fff !important;
-      background-color: default !important;
-    }
-    .app_gdpr .intro_acceptAll:hover{
-      color: default !important;
-      background-color: transparent !important;
-    }
-    .app_gdpr .button_invert {
-      color: default !important;
-      background-color: transparent !important;
-    }
-    .app_gdpr .button_invert:hover {
-      color: #fff !important;
-      background-color: default !important;
-    }
-    .app_gdpr .details_save {
-      background: default !important;
-      border-color: default !important;
-    }
-    .app_gdpr .details_save:hover {
-      color: default !important;
-      background-color: #fff !important;
-    }
-    .app_gdpr .details_button {
-      color: default !important;
-      border-color: default !important;
-    }
-    .app_gdpr .details_button:hover {
-      color: #fff !important;
-      background-color: default !important;
-    }
-    .app_gdpr .vendors_enableAll,
-    .vendors_disabled {
-      color: default !important;
-    }
-    .app_gdpr .switch_visualizationContainer,
-    .switch_visualizationGlow {
-      background-color: default !important;
-    }
-    .app_gdpr svg {
-      fill: default !important;
-    }
-  </style>
+  .app_gdpr a {
+    color: #2e7d32 !important;
+  }
+  .app_gdpr a:hover {
+    color: #133214 !important;
+  }
+  .app_gdpr button {
+    border-color: #2e7d32 !important;
+  }
+  .app_gdpr .intro_acceptAll {
+    color: #fff !important;
+    background-color: #2e7d32 !important;
+  }
+  .app_gdpr .intro_acceptAll:hover{
+    color: #2e7d32 !important;
+    background-color: transparent !important;
+  }
+  .app_gdpr .button_invert {
+    color: #2e7d32 !important;
+    background-color: transparent !important;
+  }
+  .app_gdpr .button_invert:hover {
+    color: #fff !important;
+    background-color: #2e7d32 !important;
+  }
+  .app_gdpr .details_save {
+    background: #2e7d32 !important;
+    border-color: #2e7d32 !important;
+  }
+  .app_gdpr .details_save:hover {
+    color: #2e7d32 !important;
+    background-color: #fff !important;
+  }
+  .app_gdpr .details_button {
+    color: #2e7d32 !important;
+    border-color: #2e7d32 !important;
+  }
+  .app_gdpr .details_button:hover {
+    color: #fff !important;
+    background-color: #2e7d32 !important;
+  }
+  .app_gdpr .vendors_enableAll,
+  .vendors_disabled {
+    color: #2e7d32 !important;
+  }
+  .app_gdpr .switch_visualizationContainer,
+  .switch_visualizationGlow {
+    background-color: #2e7d32 !important;
+  }
+  .app_gdpr svg {
+    fill: #2e7d32 !important;
+  }
 `;
 
 export function getColorCSS(color) {
-  return colorCSS.replace(/default/g, color);
+  return colorCSS.replace(/#2e7d32/g, color);
 }
--- reference/src/lib/init.js
+++ reference/src/lib/init.js
@@ -48,7 +48,7 @@ export function init(configUpdates) {
 			window[CMP_GLOBAL_NAME] = cmp.processCommand;
 
       // customize color if configured
-      if (config.color) {
+      if (config.color && config.color !== "#2e7d32") {
         var css = getColorCSS(config.color);
         var styleNode = document.createElement('style');
         styleNode.type = "text/css";
